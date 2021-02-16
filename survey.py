7b0471f158734f57c3d263d9446c06570c55a809
dsbowen <dsbowen@wharton.upenn.edu>
2021-02-16 18:26:48 -0500

    updated survey

--- survey.py
+++ survey.py
@@ -1,14 +1,10 @@
-import hemlock_big5 as big5
 from hemlock import Branch, Page, Label, route
+from hemlock_big5 import big5
 
 @route('/survey')
 def start():
     return Branch(
-        big5.measure_trait('Extraversion', require=True, page=True),
-        big5.measure_trait('Agreeableness', require=True, page=True),
-        big5.measure_trait('Conscientiousness', require=True, page=True),
-        big5.measure_trait('Neuroticism', require=True, page=True),
-        big5.measure_trait('Openness', require=True, page=True),
+        big5(page=True),
         Page(
             Label('The end'), 
             terminal=True
